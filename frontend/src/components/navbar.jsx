fca0ca23650c5611bfa122480fc5eb7576f906d7
Piyush <piyushgupta.pg1704@gmail.com>
2024-01-20 01:13:49 +0530

    included navbar

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="logo">
+        logo
+        </div>
+        <div className="nav-main">
+        search
+        </div>
+        <div className="nav-profile">
+        profile
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
