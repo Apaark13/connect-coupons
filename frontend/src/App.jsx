7660e2300c43fbcbf328092d7b1002e34381d63a
Apaark13 <apaark13@gmail.com>
2024-01-22 01:05:51 +0530

    users page layout and design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import Home from './components/home'
-import User from './components/user'
+import React from 'react';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import User from './components/user';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div>
+    <Router>
+      <div>
         <Navbar />
-        <User />
-    </div>
-  )
-}
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/user" element={<User/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/user.jsx
+++ frontend/src/components/user.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import './user.scss'
+import React from "react";
+import "./user.scss";
+import data from "../data"
+import Coupon from "./coupon";
 
 const User = () => {
-  return (
-    <div className='user'>
-      <div className='user-profile'>
-          profile
-      </div>
-      <div className='user-coupons'>
-          coupons
-      </div>
-    </div>
-  )
-}
+  const coupons=data.map(item => {
+    return (
+        <Coupon
+            key={item.lmd_id}
+           {...item}
 
-export default User
+            
+        />
+    )
+  });
+  return (
+    <div className="user">
+      <div className="user-profile">
+        <div className="pic">
+        <img src="/assets/user.png" alt="" />
+        </div>
+        <div className="details">
+          <h3>Dummy User</h3>
+          <div>
+            <ul>
+              <li><span>4</span>Sent</li>
+              <li><span>12</span>Received</li>
+              <li><span>23</span>Coupons</li>
+            </ul>
+          </div>
+          <section>
+            bio - Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolores distinctio deserunt enim eaque aperiam quo cupiditate aliquam! Vero possimus deserunt adipisci earum saepe enim!
+          </section>
+        </div>
+      </div>
+      <hr />
+      <div className="user-coupons">
+        <ul>
+          <li
+          //  className="active"
+           >All</li>
+          <li className="active">Sent</li>
+          <li>Received</li>
+        </ul>
+        {coupons}</div>
+    </div>
+  );
+};
+
+export default User;
