0acc03c3f7c9aaefb502449909c0d446254ab0f8
Apaark13 <apaark13@gmail.com>
2024-01-28 12:22:36 +0530

    login redirect navbar issue solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import User from './components/user';
-import Register from './components/Register';
-import Login from './components/Login';
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Footer from './components/footer';
+import React from "react";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import User from "./components/user";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Footer from "./components/footer";
 
 const App = () => {
-  const showNavbar = !window.location.pathname.includes('/login') && !window.location.pathname.includes('/register');
+  const location = useLocation();
 
+  // Condition to determine whether to show the Navbar based on the pathname instead of window using uselocation route, and for this moved the browser router over the app component
+  const showNavbar = !["/login", "/register"].includes(location.pathname);
   return (
-    <Router>
+    // <Router>
       <div>
         {showNavbar && <Navbar />}
         <Routes>
@@ -20,10 +22,12 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </div>
         {showNavbar && <Footer />}
-    </Router>
-  );
+      </div>
+
+    // </Router>
+    
+    );
 };
 
 export default App;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
    const data=await res.json()
    if(data.status==='ok')
     {
-    navigateTo('/')
+    navigateTo('/user')
    }
    console.log(data)
     }
