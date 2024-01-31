d53afdb71a1d3874ba001072fc78ba531917ac58
Piyush <piyushgupta.pg1704@gmail.com>
2024-01-31 22:51:31 +0530

    server added

--- backend/routes/coupon.js
+++ backend/routes/coupon.js
@@ -3,12 +3,22 @@ const mongoose=require('mongoose')
 const cors=require('cors')
 const router=express.Router()
 const Coupon=require('../models/coupon.model')
+const {deleteOne} = require('../models/user.model')
 
 
 router.get('/get/:email',async(req,res)=>{
-    
   try{
           const usercoupons=await Coupon.find({email:req.params.email})
+          res.send(usercoupons)
+  }
+  catch(err)
+  {
+      console.log(err)
+  }
+})
+router.get('/get',async(req,res)=>{
+  try{
+          const usercoupons=await Coupon.find()
           res.send(usercoupons)
   }
   catch(err)
@@ -31,5 +41,20 @@ router.post('/post',async(req,res)=>{
       console.log(err)
       res.json({status:'error'})
   }
+})
+router.delete('/delete/:id',async(req,res)=>{
+    try{
+          const id1=req.params.id;
+          const del=await Coupon.deleteOne({_id:id1})
+           if(del.acknowledged==true)
+           res.send({status:'ok'})
+           else
+           res.send({status:'error'})
+
+    }
+    catch(err)
+    {
+        res.send({status:'error'})
+    }
 })
 module.exports = router;
